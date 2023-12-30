aceecc5a970b85118051d393380e6d9b5f8e8f66
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-30 18:45:00 +0700

    feat: add rtc service

---
+++ src/rtc/rtc.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@nestjs/common';
+
+import { IGenerateResponse, IUser } from './interfaces';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class RtcService {
+  constructor(private prisma: PrismaService) {}
+
+  createSocketRoom() {
+    return this.prisma['socket_room'].create({
+      data: {
+        isActive: true,
+      },
+      select: {
+        roomId: true,
+        createdAt: true,
+        isActive: true,
+      },
+    });
+  }
+
+  insertSocketUser(createUser: IUser) {
+    return this.prisma['socket_user'].create({
+      data: {
+        name: createUser.name,
+        roomId: createUser.roomId,
+        socketId: createUser.socketId,
+      },
+      select: {
+        roomId: true,
+        createdAt: true,
+      },
+    });
+  }
+
+  queryUserBySocketId(socketId: string) {
+    return this.prisma['socket_user'].findFirst({
+      where: {
+        socketId,
+      },
+      select: {
+        userId: true,
+        roomId: true,
+        socketId: true,
+        name: true,
+      },
+    });
+  }
+
+  removeUserByUserId(socketId: string) {
+    return this.prisma['socket_user'].delete({
+      where: {
+        userId: socketId,
+      },
+      select: {
+        roomId: true,
+        socketId: true,
+        name: true,
+      },
+    });
+  }
+
+  queryUsersByRoomId(roomId: string) {
+    return this.prisma['socket_user'].findMany({
+      where: {
+        roomId: roomId,
+      },
+      select: {
+        userId: true,
+        name: true,
+        socketId: true,
+      },
+    });
+  }
+
+  generateResponse(
+    success: boolean = true,
+    message: string = '',
+  ): IGenerateResponse {
+    return {
+      success,
+      message,
+    };
+  }
+
+  async checkRoomAvailability(roomId: string): Promise<any> {
+    return this.prisma['socket_room'].findFirst({
+      where: {
+        roomId: roomId,
+      },
+      select: {
+        roomId: true,
+        isActive: true,
+      },
+    });
+  }
+}
