3d13f993be8a29e49eb78441d22dc7d26e4185f4
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-30 18:45:18 +0700

    feat: update rtc gateway

--- src/rtc/rtc.gateway.ts
+++ src/rtc/rtc.gateway.ts
@@ -1,20 +1,91 @@
 import {
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Socket, Server } from 'socket.io';
 
-@WebSocketGateway(80, {
+import type { ICreateRoom, IJoinRoom } from './interfaces';
+import { RtcService } from './rtc.service';
+
+@WebSocketGateway(50052, {
   cors: {
     origin: '*',
   },
 })
-export class RtcGateway {
+export class RtcGateway implements OnGatewayDisconnect {
   @WebSocketServer() server: Server;
 
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): void {
-    this.server.emit('message', 'Hello world!');
+  constructor(private readonly rtcService: RtcService) {}
+
+  @SubscribeMessage('create-new-room')
+  async handleCreateNewRoom(
+    client: Socket,
+    payload: ICreateRoom,
+  ): Promise<void> {
+    const socketRoom = await this.rtcService.createSocketRoom();
+    await this.rtcService.insertSocketUser({
+      name: payload.meetingName,
+      roomId: socketRoom.roomId,
+      socketId: client.id,
+    });
+
+    client.join(socketRoom.roomId);
+
+    client.emit('room-id', {
+      success: true,
+      socketId: client.id,
+      roomId: socketRoom.roomId,
+    });
+
+    const connectedUsers = await this.rtcService.queryUsersByRoomId(
+      socketRoom.roomId,
+    );
+
+    client.emit('room-users', {
+      connectedUsers,
+    });
+  }
+
+  @SubscribeMessage('join-room')
+  async handleJoinRoom(client: Socket, payload: IJoinRoom): Promise<void> {
+    await this.rtcService.insertSocketUser({
+      name: payload.meetingName,
+      roomId: payload.meetingId,
+      socketId: client.id,
+    });
+
+    client.join(payload.meetingId);
+
+    client.emit('room-id', {
+      success: true,
+      socketId: client.id,
+      roomId: payload.meetingId,
+    });
+
+    const connectedUsers = await this.rtcService.queryUsersByRoomId(
+      payload.meetingId,
+    );
+
+    this.server.volatile.to(payload.meetingId).emit('room-users', {
+      connectedUsers,
+    });
+  }
+
+  async handleDisconnect(client: Socket): Promise<void> {
+    const getUser = await this.rtcService.queryUserBySocketId(client.id);
+
+    await this.rtcService.removeUserByUserId(getUser.userId);
+
+    client.leave(getUser.roomId);
+
+    const connectedUsers = await this.rtcService.queryUsersByRoomId(
+      getUser.roomId,
+    );
+
+    this.server.volatile.to(getUser.roomId).emit('room-users', {
+      connectedUsers,
+    });
   }
 }
