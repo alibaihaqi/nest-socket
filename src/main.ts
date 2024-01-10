4ca17af0705bcb5ea129c72eab6852ae62707b6a
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-10 17:16:02 +0700

    feat: add enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useWebSocketAdapter(new IoAdapter(app));
+  app.enableCors();
   await app.listen(parseInt(process.env.APPLICATION_PORT) || 3000);
 }
 bootstrap();
